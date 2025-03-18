a035ae3566d29f0d67de91526905356f09d68128
AjayRathode6703 <ar5035211@gmail.com>
2025-03-18 13:11:10 +0530

    "change in routes"

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import routes from "./router/student.js"
 import { connectdb } from "./db/connectdb.js"
 let app=express()
 app.use(express.json())
-app.use("/student",routes)
+app.use("/",routes)
 
 connectdb().then (()=>{
     app.listen(4001,()=>console.log("server started at  4001"))
